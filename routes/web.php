453e80b988a00898a55b87e89bb8417355f502e1
arvydux <arvydas.kavaliauskas83@gmail.com>
2025-04-29 02:01:56 +0300

    mijmij

--- routes/web.php
+++ routes/web.php
@@ -5,94 +5,55 @@ use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
 use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardMarkup;
 use SergiX44\Nutgram\Telegram\Types\Message\Message;
+use Telegram\Bot\Keyboard\Keyboard;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
+Route::post('/webhook', function() {
+    $updates = Telegram::getWebhookUpdate();
 
+    return 'ok';
 
-Route::post('/webhook', function () {
-    $bot = new Nutgram($_ENV['TELEGRAM_TOKEN']);
-
-    dd($bot);
-    // Handle the /start command
-    $bot->onCommand('start', function (Nutgram $bot) {
-        $bot->sendMessage('Welcome to the bot! Use /help to see available commands.');
-    });
-
-    // Handle the /help command
-    $bot->onCommand('help', function (Nutgram $bot) {
-        $bot->sendMessage("Here are the available commands:\n/start - Start the bot\n/help - Show this help message\n/about - Learn more about the bot");
-    });
-
-    // Handle the /about command
-    $bot->onCommand('about', function (Nutgram $bot) {
-        $bot->sendMessage('This is a sample Telegram bot built using Nutgram.');
-    });
-
-    // Process the incoming update
-    $bot->run();
-
-
-    return response('OK');
-})->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
-
-Route::get('/webhook', function() {
-    $bot = new Nutgram($_ENV['TELEGRAM_TOKEN']);
-
-
-        $bot->sendMessage(
-            text: 'Choose an option:',
-            reply_markup: InlineKeyboardMarkup::make()->addRow(
-                InlineKeyboardButton::make('One', callback_data: 'number 1'),
-                InlineKeyboardButton::make('Two', callback_data: 'number 2'),
-                InlineKeyboardButton::make('Cancel', callback_data: 'cancel'),
-            )
-        );
-
-
-    $bot->onCallbackQueryData('number {param}', function (Nutgram $bot, $param) {
-        $bot->sendMessage($param); // 1 or 2
-        $bot->answerCallbackQuery();
-    });
-
-    $bot->onCallbackQueryData('cancel', function (Nutgram $bot) {
-        $bot->sendMessage('Canceled!');
-        $bot->answerCallbackQuery();
-    });
-
-    $bot->run();
-
-    return view('welcome');
 })->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
 
 Route::get('/', function() {
-    $bot = new Nutgram($_ENV['TELEGRAM_TOKEN']);
 
+   // $response = Telegram::bot('Arvi_bot')->getMe();
 
-        $bot->sendMessage(
-            text: 'Choose an option:',
-            chat_id: 2091649713,
-            reply_markup: InlineKeyboardMarkup::make()->addRow(
-                InlineKeyboardButton::make('One', callback_data: 'number 1'),
-                InlineKeyboardButton::make('Two', callback_data: 'number 2'),
-                InlineKeyboardButton::make('Cancel', callback_data: 'cancel'),
-            )
-        );
+    $response = Telegram::sendMessage([
+        'chat_id' => '2091649713',
+        'text' => 'Hello Worldjkjjkmokmokmokmokmojkm'
+    ]);
 
+    $reply_markup = Keyboard::make()
+        ->setResizeKeyboard(true)
+        ->setOneTimeKeyboard(true)
+        ->row([
+            Keyboard::button('1'),
+            Keyboard::button('2'),
+            Keyboard::button('3'),
+        ])
+        ->row([
+            Keyboard::button('4'),
+            Keyboard::button('5'),
+            Keyboard::button('6'),
+        ])
+        ->row([
+            Keyboard::button('7'),
+            Keyboard::button('8'),
+            Keyboard::button('9'),
+        ])
+        ->row([
+            Keyboard::button('0'),
+        ]);
 
-    $bot->onCallbackQueryData('number {param}', function (Nutgram $bot, $param) {
-        $bot->sendMessage(
-            text: 'Choose an h7777777option:',
-            chat_id: 2091649713);
-        $bot->answerCallbackQuery();
-    });
+    $response = Telegram::sendMessage([
+        'chat_id' => '2091649713',
+        'text' => 'Hello World',
+        'reply_markup' => $reply_markup
+    ]);
 
-    $bot->onCallbackQueryData('cancel', function (Nutgram $bot) {
-        $bot->sendMessage(
-            text: 'Choose an optihhhhhhhhon:',
-            chat_id: 2091649713);
-        $bot->answerCallbackQuery();
-    });
+    $messageId = $response->getMessageId();
 
-   // $bot->run();
 
     return view('welcome');
 })->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
