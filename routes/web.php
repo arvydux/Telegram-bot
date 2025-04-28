f1d409a46540a0407aa8725f72309c28427011b6
arvydux <arvydas.kavaliauskas83@gmail.com>
2025-04-28 23:37:27 +0300

    nhunhu

--- routes/web.php
+++ routes/web.php
@@ -7,37 +7,6 @@ use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardMarkup;
 use SergiX44\Nutgram\Telegram\Types\Message\Message;
 
 
-Route::get('/', function () {
-    $bot = new Nutgram($_ENV['TELEGRAM_TOKEN']);
-
-
-        $bot->sendMessage(
-            text: 'Welcome!',
-            chat_id: 2091649713,
-            reply_markup: InlineKeyboardMarkup::make()
-                ->addRow(
-                    InlineKeyboardButton::make('A', callback_data: 'type:a'),
-                    InlineKeyboardButton::make('B', callback_data: 'type:b')
-                ),
-            //chat_id: $_ENV['TELEGRAM_CHAT_ID']
-        );
-
-
-    $bot->onCommand('type:a', function(Nutgram $bot){
-        $bot->sendMessage(
-            text: 'You selected A'
-        );
-    });
-
-    $bot->onCommand('type:b', function(Nutgram $bot){
-        $bot->sendMessage(
-            text: 'You selected B'
-        );
-    });
-
-    return view('welcome');
-})->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
-
 Route::post('/webhook', function() {
     $bot = new Nutgram($_ENV['TELEGRAM_TOKEN']);
 
