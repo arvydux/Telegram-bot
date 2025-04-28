0ed7cea51680da6c0a6d5d46644c31bdd163610c
arvydux <arvydas.kavaliauskas83@gmail.com>
2025-04-29 02:24:05 +0300

    ki9ki

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
@@ -10,12 +11,37 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 Route::post('/webhook', function() {
     $updates = Telegram::getWebhookUpdate();
-
+    $response = Telegram::sendMessage([
+        'chat_id' => '2091649713',
+        'text' => 90,
+    ]);
     return 'ok';
 
 })->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
 
 Route::get('/', function() {
+
+
+
+
+/*    $response = Http::withHeaders([
+        'Content-Type' => 'application/json',
+    ])->post('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?
+    key=AIzaSyBtbX4g8tyHKL2YbYce2IUo6_oF4EDK24A', [
+        'contents' => [
+            [
+                'parts' => [
+                    ['text' => 'Explain how AI works']
+                ]
+            ]
+        ]
+    ]);
+
+    if ($response->successful()) {
+        return $response->json();
+    } else {
+        return $response->body();
+    }*/
 
    // $response = Telegram::bot('Arvi_bot')->getMe();
 
@@ -62,6 +88,6 @@ Route::get('/', function() {
 Route::get('/set-webhook', function () {
     $bot = new Nutgram($_ENV['TELEGRAM_TOKEN']);
     $bot->setWebhook('https://telegram-bot-master-pyrd6s.laravel.cloud/webhook');
-    return 'Webhook set successfully!';
+    return 'ooWebhook set successfully!';
 })->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
 
