82a240616fd56e0d0c6b4a6316cf439d8129c811
arvydux <arvydas.kavaliauskas83@gmail.com>
2025-04-29 13:28:04 +0300

    hunji

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::post('/webhook', function(Request $request) {
 
     $response = Telegram::sendMessage([
         'chat_id' => '2091649713',
-        'text' => json_encode($request->all()) . '-' . json_encode($updates->all())    ]);
+        'text' => 'test' . json_encode($request->all()) . '-' . json_encode($updates->all())    ]);
     return 'ok';
 
 })->withoutMiddleware([\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
